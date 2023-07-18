a7358ec10a39ad4a45ac11ee5661682df4fff728
Denzel <jdenzelmab@gmail.com>
2023-07-18 19:39:41 -0400

    Completed anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:   
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matched_list = []
+        for word in word_list:
+            if sorted(word) == sorted(self.word):
+                matched_list.append(word)
+        return matched_list
